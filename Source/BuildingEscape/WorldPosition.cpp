b9c3c98414fd658f199665a1cc7564eec165eb4d
WongDarren <wongdarren1997@gmail.com>
2021-01-16 17:53:23 -0800

    init project and add door and texture assets

---
+++ Source/BuildingEscape/WorldPosition.cpp
@@ -0,0 +1,43 @@
+// Copyright 2021 Darren Wong
+
+#include "WorldPosition.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UWorldPosition::UWorldPosition()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+// Called when the game starts
+void UWorldPosition::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	// FString Log = TEXT("Hello!");
+	// FString *PtrLog = &Log;
+
+	// Log.Len();
+	// (*PtrLog).Len();
+	// PtrLog->Len();
+
+	// UE_LOG(LogTemp, Warning, TEXT("%s"), **PtrLog);
+
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPosition = GetOwner()->GetActorLocation().ToString();
+
+	UE_LOG(LogTemp, Warning, TEXT("%s's position in world is:  %s"), *ObjectName, *ObjectPosition);
+}
+
+// Called every frame
+void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
